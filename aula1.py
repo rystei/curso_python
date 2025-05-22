460a01f10ed1fc541c479ee976281fd6afcab133
rystei <gustavo.telles@edu.unifil.br>
2025-05-22 10:37:21 -0300

    aula2

--- aula1.py
+++ aula1.py
@@ -1,2 +1,13 @@
+"""
+DocString
+e escrever o que quiser
+"""
+
+'''
+Usar para escrever suas notas ou explicar meu código
+'''
+
+#Permite escrever um comentário
 print("Hello, world!")
+#aqui
 print("atençao")
---
+++ aula2.py
@@ -0,0 +1,8 @@
+#Print() usado para imprimir as coisas na tela desde argumentos, objetos e funções.
+#(sep=) é responsalvel por alterar o separador dos argumentos é um = (argumento nomeado).
+#(end=) é responsalvel por alterar o final do print é um = (argumento nomeado).
+# \n -> LF
+# Sempre ler as mensagens de erros o python é bem didatico em questões de apresentar erros.
+print(12, 34, sep=" ", end='\n')
+print(56, 78, sep='//', end='___final_alterado \n')
+print(1234, 7345, sep='///')
