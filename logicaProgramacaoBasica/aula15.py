982664daaaaefccf3d9eb1641c357627612a09f4
Gustavo Cardoso Telles <gustavo.telles@edu.unifil.br>
2025-07-17 13:25:11 -0300

    input

---
+++ logicaProgramacaoBasica/aula15.py
@@ -0,0 +1,8 @@
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome}')
+
+numero_1 = int(input('digite um número: '))
+numero_2 = int(input('digite outro número: '))
+
+print(f'A soma dos números é {numero_1 + numero_2}')
+
