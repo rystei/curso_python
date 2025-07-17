fa483f5b7b6e47e3d0c18298a9f9fc6fcf04d7c0
Gustavo Cardoso Telles <gustavo.telles@edu.unifil.br>
2025-07-17 09:07:48 -0300

    imc

---
+++ aula12.py
@@ -0,0 +1,7 @@
+nome = 'rystei'
+altura = 1.71
+peso = 68
+imc = peso / (altura * altura)
+print(nome , 'tem' , altura , 'de altura,')
+print('pesa', peso, 'quilos e seu IMC é')
+print(round(imc,2))
