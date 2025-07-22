cf6581fe48b7dee6a448cebb85d345fa4ad1688b
rystei <gustavo.telles@edu.unifil.br>
2025-07-22 09:29:16 -0300

    reforçando if/elif/else

---
+++ logicaProgramacaoBasica/aula17.py
@@ -0,0 +1,21 @@
+#if/ elif       / else
+#se/ se não se  / se não
+
+condicao1 = False
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Esse é o código da condição 1')
+elif condicao2:
+    print('Esse é o código da condição 2')
+elif condicao3:
+    print('Esse é o código da condição 3')
+elif condicao4:
+    print('Esse é o código da condição 4') 
+else:
+    print('Nenhuma condição foi satisfeita')
+    
+    
+print('Fora do if')
