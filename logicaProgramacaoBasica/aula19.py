92483558d9a8377f09939f6037b761bdd52ce2f2
rystei <gustavo.telles@edu.unifil.br>
2025-07-22 10:17:54 -0300

    Operadores de comparação (Relacionais)

---
+++ logicaProgramacaoBasica/aula19.py
@@ -0,0 +1,18 @@
+"""
+Operadores de comparação (Relacionais)
+Op      significado     exemplo (True)
+>       maior           2 > 1
+>=      maior ou igual  2 >=2
+<       menor           1 < 2
+<=      menor ou igual  2 <= 2
+==      igual           'a' == 'a'
+!=      diferente       'a' != 'B'
+"""
+maior = 2 > 1
+maior_ou_igual = 4 >= 4
+menor = 1 < 10
+menor_ou_igual = 5 <= 6 
+igual = '12' == '12'
+diferente = 'Eu sou' != 'I`am'
+print(diferente)
+print(igual)
